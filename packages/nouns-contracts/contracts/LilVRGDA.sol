ca9d0f97b391889607004d864deaf55f787a3c6d
nvonpentz-dao <113955389+nvonpentz-dao@users.noreply.github.com>
2022-11-27 16:41:15 -0500

    Add LilVRGDA contracts and tests

--- packages/nouns-contracts/contracts/LilVRGDA.sol
+++ packages/nouns-contracts/contracts/LilVRGDA.sol
@@ -1,82 +1,181 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.17;
 
 import { LinearVRGDA } from "VRGDAs/LinearVRGDA.sol";
 import { NounsToken } from "contracts/NounsToken.sol";
 import { INounsSeeder } from "contracts/interfaces/INounsSeeder.sol";
 import { INounsDescriptor } from "contracts/interfaces/INounsDescriptor.sol";
+import { ILilVRGDA } from "contracts/interfaces/ILilVRGDA.sol";
+
+import { Pausable } from '@openzeppelin/contracts/security/Pausable.sol';
+import { ReentrancyGuard } from '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 
 import { IWETH } from "contracts/interfaces/IWETH.sol";
-
 import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-
 import { toDaysWadUnsafe } from "solmate/utils/SignedWadMath.sol";
+// import { console } from "forge-std/console.sol";
 
-contract LilVRGDA is LinearVRGDA {
-    uint256 public nounIdLastSale; // The total number sold
-    uint256 public immutable startTime = block.timestamp; // When VRGDA sales begun.
+// Functions should be grouped according to their visibility and ordered:
+// * constructor
+// * receive function (if exists)
+// * fallback function (if exists)
+// * external
+// * public
+// * internal
+// * private
+// sub order is (state-modifiable, then view, then pure within each group)
 
-    NounsToken public immutable nouns;
-    address public immutable weth;
 
-    constructor(uint256 _nounId, address _nounsTokenAddress, address _weth)
-        LinearVRGDA(
-            69.42e18, // Target price.
-            0.31e18, // Price decay percent.
-            2e18 // Per time unit.
-        )
-    {
-        nounIdLastSale = _nounId;
-        weth = _weth;
-        nouns = NounsToken(_nounsTokenAddress);
+// contract LilVRGDA is ILilVRGDA, LinearVRGDA  { // TODO review how the order of inheritance matters
+contract LilVRGDA is ILilVRGDA, LinearVRGDA, Pausable, ReentrancyGuard, Ownable {
+    uint256 public nextNounId; // The total number sold + 1
+    uint256 public immutable startTime; // When VRGDA sales begun.
+    uint256 public immutable updateInterval = 15 minutes;
+
+    // The minimum price accepted in an auction
+    uint256 public reservePrice;
+
+    NounsToken public immutable nounsToken;
+    address public immutable wethAddress;
+
+    constructor(
+        int256 _targetPrice,
+        int256 _priceDecayPercent,
+        int256 _perTimeUnit,
+        uint256 _nextNounId,
+        uint256 _startTime,
+        address _nounsTokenAddress,
+        address _wethAddress,
+        uint256 _reservePrice
+    ) LinearVRGDA(_targetPrice, _priceDecayPercent, _perTimeUnit) {
+        nounsToken = NounsToken(_nounsTokenAddress);
+        nextNounId = _nextNounId;
+        startTime = _startTime;
+        wethAddress = _wethAddress;
+        reservePrice = _reservePrice;
     }
 
-    function fetchNextNoun() external view returns (string memory svg) {
+    // TODO initialize function
+    function settleAuction(uint expectedNounId, bytes32 expectedParentBlockhash) external payable override whenNotPaused nonReentrant {
+        // Only settle if desired Noun would be minted
+        bytes32 parentBlockhash = blockhash(block.number - 1);
+        require(expectedParentBlockhash == parentBlockhash, "Invalid or expired blockhash");
+        uint _nextNounIdForCaller = nextNounIdForCaller();
+        require(expectedNounId == _nextNounIdForCaller, "Invalid or expired nounId");
+        require(msg.value >= reservePrice, "Below reservePrice");
+
+        // Validate the purchase request against the VRGDA rules.
+        // uint256 price = getVRGDAPrice(toDaysWadUnsafe(block.timestamp - startTime), nextNounId);
+        uint256 price = getCurrentVRGDAPrice();
+        require(msg.value >= price, "Insufficient funds");
+
+        // Call settleAuction on the nouns contract.
+        uint256 mintedNounId = nounsToken.mint();
+        assert(mintedNounId == _nextNounIdForCaller);
+
+        // Sends token to caller.
+        nounsToken.transferFrom(address(this), msg.sender, mintedNounId);
+
+        // Sends the funds to the DAO.
+        if (msg.value > 0) {
+            uint refundAmount = msg.value - price;
+            if (refundAmount > 0) {
+                _safeTransferETHWithFallback(msg.sender, refundAmount);
+            }
+            if (price > 0) {
+                _safeTransferETHWithFallback(address(2), price); // TODO replace with DAO address or make ownable by DAO
+            }
+        }
+
+        nextNounId = mintedNounId+1;
+        emit AuctionSettled(mintedNounId, msg.sender, price);
+    }
+
+    /**
+     * @notice Set the auction reserve price.
+     * @dev Only callable by the owner.
+     */
+    function setReservePrice(uint256 _reservePrice) external onlyOwner {
+        reservePrice = _reservePrice;
+
+        emit AuctionReservePriceUpdated(_reservePrice);
+    }
+
+    /**
+     * @notice Pause the Nouns auction house.
+     * @dev This function can only be called by the owner when the
+     * contract is unpaused. While no new auctions can be started when paused,
+     * anyone can settle an ongoing auction.
+     */
+    function pause() external override onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @notice Unpause the Nouns auction house.
+     * @dev This function can only be called by the owner when the
+     * contract is paused. If required, this function will start a new auction.
+     */
+    function unpause() external override onlyOwner {
+        _unpause();
+    }
+
+    function fetchNextNoun() external view override returns (
+        uint256 nounId,
+        INounsSeeder.Seed memory seed,
+        string memory svg,
+        uint256 price,
+        bytes32 hash
+    ) {
+        uint _nextNounIdForCaller = nextNounIdForCaller();
         // Generate the seed for the next noun.
-        INounsSeeder seeder = INounsSeeder(nouns.seeder());
-        INounsDescriptor descriptor = INounsDescriptor(nouns.descriptor());
-        INounsSeeder.Seed memory nextNounSeed = seeder.generateSeed(
-            nounIdLastSale + 1,
+        INounsSeeder seeder = INounsSeeder(nounsToken.seeder());
+        INounsDescriptor descriptor = INounsDescriptor(nounsToken.descriptor());
+        seed = seeder.generateSeed(
+            _nextNounIdForCaller,
             descriptor
         );
 
         // Generate the SVG from seed using the descriptor.
-        svg = descriptor.generateSVGImage(nextNounSeed);
+        svg = descriptor.generateSVGImage(seed);
 
-        // TODO also return expected blockhash
-        return svg;
+        // Calculate price based on VRGDA rules.
+        price = getCurrentVRGDAPrice();
+
+        // Fetch the blockhash associated with this noun.
+        hash = blockhash(block.number-1);
+
+        return (_nextNounIdForCaller, seed, svg, price, hash);
     }
 
-    function mint(uint256 invalidAfter) external payable {
-        // Check the blockhash is not expired. TODO - use blockhash not deadline.
-        require(block.number <= invalidAfter, "Lil Noun expired.");
+    // Note: I can keep this function and still test by having my
+    // tests inherit from this contract
+    function getCurrentVRGDAPrice() public view returns (uint256) {
+        uint absoluteTimeSinceStart = block.timestamp - startTime;
+        return getVRGDAPrice(toDaysWadUnsafe(absoluteTimeSinceStart - (absoluteTimeSinceStart % updateInterval)), nextNounId);
+    }
 
-        // Validate the purchase request against the VRGDA rules.
-        uint256 price = getVRGDAPrice(toDaysWadUnsafe(block.timestamp - startTime), nounIdLastSale);
-        require(msg.value >= price);
-
-        // Call mint on the nouns contract.
-        uint mintedNounId = nouns.mint();
-
-        // Sends token to user.
-        nouns.transferFrom(address(this), msg.sender, mintedNounId);
-
-        // Sends the funds to the DAO.
-        if (msg.value > 0) {
-            _safeTransferETHWithFallback(0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B, msg.value); // TODO replace with DAO address or make ownable by DAO
+    // @dev handles edge case in nouns token contract
+    function nextNounIdForCaller() public view returns (uint256) {
+        // Calculate nounId that would be minted to the caller
+        uint _nextNounIdForCaller = nextNounId;
+        if (_nextNounIdForCaller <= 175300  && _nextNounIdForCaller % 10 == 0) {
+            _nextNounIdForCaller++;
         }
-
-        nounIdLastSale = mintedNounId+1;
-        // emit NounPurchased(mintedNounId, msg.sender, msg.value);
+        if (_nextNounIdForCaller <= 175301 && _nextNounIdForCaller % 10 == 1) {
+            _nextNounIdForCaller ++;
+        }
+        return _nextNounIdForCaller;
     }
 
     /**
-     * @notice Transfer ETH. If the ETH transfer fails, wrap the ETH and try send it as WETH.
+     * @notice Transfer ETH. If the ETH transfer fails, wrap the ETH and try send it as wethAddress.
      */
     function _safeTransferETHWithFallback(address to, uint256 amount) internal {
         if (!_safeTransferETH(to, amount)) {
-            IWETH(weth).deposit{ value: amount }();
-            IERC20(weth).transfer(to, amount);
+            IWETH(wethAddress).deposit{ value: amount }();
+            IERC20(wethAddress).transfer(to, amount);
         }
     }
 
@@ -89,5 +188,3 @@ contract LilVRGDA is LinearVRGDA {
         return success;
     }
 }
-
-
